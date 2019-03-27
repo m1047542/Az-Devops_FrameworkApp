9b0a9d40b9937431a8474fbdcabacf3de07485ef
M1047542 <M1047542@mindtree.com>
2019-03-27 12:25:55 +0530

    Add project files.

---
+++ AZ_devops_FrameworkWebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AZ_devops_FrameworkWebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AZ_devops_FrameworkWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AZ_devops_FrameworkWebApp.MvcApplication" Language="C#" %>
